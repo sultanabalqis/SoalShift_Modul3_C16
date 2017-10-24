d19e26dd899172e0d9ab9a9e9b180b1af1ea3743
dnirmalasari18 <dnirmalasari1@gmail.com>
2017-10-24 23:10:38 +0700

    tambah stok penjual

--- soal1.c
+++ soal1.c
@@ -6,42 +6,47 @@
 
 //pthread_t tid[6];
 int senjata[6]={10,10,10,10,10,10};
-
+void pembeli();
 void penjual(){
-	int pilihan;	
+	int pilihan=0;	
 //	system ("clear");
 	printf("SEBAGAI PENJUAL\n");
 	printf("1. Lihat Stock Senjata\n");
 	printf("2. Tambah Stock Senjata\n");
-	printf("PILIH: ");
-	while(scanf("%d",&pilihan)){
-		if (pilihan==1){
-			system ("clear");
-			int i;
-			printf("STOK SENJATA\n\n");
-			for(i=0;i<6;i++){
-				//if(senjata[i]==0) continue;
-				if(i==0) printf("MP4A1 %d\n",senjata[i]);
-				else if(i==1) printf("PM2-V1 %d\n",senjata[i]);
-				else if(i==2) printf("SPR-3 %d\n",senjata[i]);
-				else if(i==3) printf("SS2-V5 %d\n",senjata[i]);
-				else if(i==4) printf("SPG1-V3 %d\n",senjata[i]);
-				else if(i==5) printf("MINE %d\n", senjata[i]);		
-			}
+	printf("PILIH: "); scanf("%d ",&pilihan);
+	
+	if (pilihan==1){
+//		system ("clear");
+		int i;
+		printf("STOK SENJATA\n\n");
+		for(i=0;i<6;i++){
+			//if(senjata[i]==0) continue;
+			if(i==0) printf("MP4A1 %d\n",senjata[i]);
+			else if(i==1) printf("PM2-V1 %d\n",senjata[i]);
+			else if(i==2) printf("SPR-3 %d\n",senjata[i]);
+			else if(i==3) printf("SS2-V5 %d\n",senjata[i]);
+			else if(i==4) printf("SPG1-V3 %d\n",senjata[i]);
+			else if(i==5) printf("MINE %d\n", senjata[i]);		
 		}
-
-		else if (pilihan==2){
-		//	system ("clear");
-			char masuk[8];
-			int input;
-			scanf("%s %d", masuk,&input);
-			if(masuk=="MP4A1") senjata[0]+=input;		
-		}
-		else if (pilihan==3) break;
-
+		printf("\n\n");
+		penjual();
 	}
-	if(pilihan==3) printf("hehe\n");
+
+	else if (pilihan==2){
+	//	system ("clear");
+		char masuk[8];
+		int input;
+		scanf("%s %d", masuk,&input);
+		if(masuk=="MP4A1") senjata[0]+=input;
+		penjual();	
+	}
+		
+	if(pilihan==3) pembeli();
 }	
+
+void pembeli(){
+	printf("cie masuk pembeli\n");
+}
 
 int main(){
 
@@ -57,9 +62,9 @@ int main(){
 	printf("INPUT: ");
 	scanf("%d",&temp);
 	
-	while(1){
+//	while(1){
 		if(temp==1) penjual();
 		//else if(temp==2) pembeli;
-	}
+//	}
 	return 0;
 }
