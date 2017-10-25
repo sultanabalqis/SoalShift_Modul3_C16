270a8f2bfb58ed9288cd11f61a0b8105a6f7155e
dnirmalasari18 <dnirmalasari1@gmail.com>
2017-10-25 08:09:48 +0700

    revisi

--- soal5.c
+++ soal5.c
@@ -3,21 +3,40 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
-pthread_t tid1,tid2;
- 
+
+pthread_t tid1;
+pthread_t tid2;
+int status;
+FILE* novel;
 void* cariFina(void *arg){
-    	printf("cie masuk ke fungsi ");
+	//status=0;
+	novel= fopen("Novel.txt","r");
+	int count=0;
+	char isi[1000];
+	while(fscanf(novel,"%s",isi) != EOF){
+ 	   	if(strstr(isi,"fina")!=NULL) count++;
+		//status=1;
+	}
     	return NULL;
 }
 
 void* cariIfah(void *arg){
-	printf("cie cie");
+	//while(status!=1){
+	
+	//}
+	novel= fopen("Novel.txt","r");
+	int count=0;
+	char isi[1000];
+	while(fscanf(novel,"%s",isi) != EOF){
+ 	   	if(strstr(isi,"ifah")!=NULL) count++;//status=1;
+	}
+    	printf("iniIfah %d\n",count);
 	return NULL;
 }
 int main(){
 
-	pthread_create(&(tid1), NULL, &cariFina, NULL);
-	pthread_create(&(tid2),NULL, &cariIfah,NULL);
+	pthread_create(&(tid1), NULL, &cariIfah, NULL);
+	pthread_create(&(tid2),NULL, &cariFina,NULL);
     	pthread_join(tid1, NULL);
     	pthread_join(tid2, NULL);
  
