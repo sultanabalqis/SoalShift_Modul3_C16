0cc98b355c4b927a65516a8b884a5c8e4a872154
dnirmalasari18 <dnirmalasari1@gmail.com>
2017-10-25 11:04:40 +0700

    kasih makan

--- soal3.c
+++ soal3.c
@@ -5,7 +5,9 @@
 #include<unistd.h>
 #include<stdlib.h>
 
+pthread_t tid[4];
 int hewan[2];
+
 void* lohangabut(void *end){
 	while(1){
 		sleep(10);
@@ -16,18 +18,38 @@ void* lohangabut(void *end){
 void* kepitinggabut(void *end){
 	while(1){
 		sleep(20);
-		hewan[0]-=20;
+		hewan[1]-=20;
 	}
 }
 
+void* kepitingmakan(void *end){
+	hewan[1]+=10;
+}
+
+void* lohanmakan(void *end){
+	hewan[0]+=10;
+}
 int main(void){
-    hewan[0]=100; //lohan
-    hewan[1]=100; //kepiting
+	hewan[0]=100; //lohan
+	hewan[1]=100; //kepiting
+	int pilih;
+	pthread_create(&(tid[0]),NULL,lohangabut,NULL);
+	pthread_create(&(tid[1]),NULL,kepitinggabut,NULL);
+	printf("PILIHAN KEGIATAN:\n");
+	printf("1. Beri makan Lohan\n");
+	printf("2. Beri makan Kepiting\n");
 
-    pthread_t tid[2];
+	while(scanf("%d",&pilih)){
+		if(pilih==1){
+			pthread_create(&(tid[2]),NULL,lohanmakan,NULL);		
+		}
+		else if(pilih==2){
+			pthread_create(&(tid[3]),NULL,kepitingmakan,NULL);		
+		}
+		int i;
+		printf("Status Lohan: %d\n", hewan[0]);
+		printf("Status Kepiting: %d\n",hewan[1]);
 
-    pthread_create(&(tid[0]),NULL,&lohangabut,NULL);
-    pthread_create(&(tid[1]),NULL,&kepitinggabut,NULL);
-
-    return 0;
+	}
+	return 0;
 }
