5165d6d70ba3c7c092566de73ce98ab62e22e711
dnirmalasari18 <32873856+dnirmalasari18@users.noreply.github.com>
2017-11-04 07:58:43 +0700

    Add files via upload
    ter-fix

--- soal3.c
+++ soal3.c
@@ -5,7 +5,7 @@
 #include<unistd.h>
 #include<stdlib.h>
 
-pthread_t tid[4];
+pthread_t tid[5];
 int hewan[2];
 
 void* lohangabut(void *end){
@@ -29,6 +29,11 @@ void* kepitingmakan(void *end){
 void* lohanmakan(void *end){
 	hewan[0]+=10;
 }
+
+void* cekstatus(void *end){
+        printf("Status Lohan: %d\n", hewan[0]);
+		printf("Status Kepiting: %d\n",hewan[1]);
+}
 int main(void){
 	hewan[0]=100; //lohan
 	hewan[1]=100; //kepiting
@@ -38,18 +43,19 @@ int main(void){
 	printf("PILIHAN KEGIATAN:\n");
 	printf("1. Beri makan Lohan\n");
 	printf("2. Beri makan Kepiting\n");
+    printf("3. Cek Status Saat Ini\n");
 
 	while(scanf("%d",&pilih)){
-		if(pilih==1){
-			pthread_create(&(tid[2]),NULL,lohanmakan,NULL);		
+        if(pilih==1){
+			pthread_create(&(tid[2]),NULL,lohanmakan,NULL);	
 		}
 		else if(pilih==2){
 			pthread_create(&(tid[3]),NULL,kepitingmakan,NULL);		
 		}
-		int i;
-		printf("Status Lohan: %d\n", hewan[0]);
-		printf("Status Kepiting: %d\n",hewan[1]);
-		if(hewan[0]>100 || hewan[1]>100 ||hewan[0]<0||hewan[1]<0){
+        else if(pilih==3){
+            pthread_create(&(tid[4]),NULL,cekstatus,NULL);
+        }
+        if(hewan[0]>100 || hewan[1]>100 ||hewan[0]<0||hewan[1]<0){
 			printf("===GAME TELAH BERAKHIR===\n\n\n");
 			break;
 		}
--- soal5.c
+++ soal5.c
@@ -4,31 +4,29 @@
 #include<stdlib.h>
 #include<unistd.h>
 
+pthread_t ttt[10]; 
 
-void *cari(void *end){
+void* cari(void *end){
 	FILE *baca;
-	baca=fopen("/home/nirmalasari/SoalShift_Modul3_C16/Novel.txt","r");
-	char nama[5],isi[300];
+	baca=fopen("/home/dell/SoalShift_Modul3_C16/Novel.txt","r");
+    char nama[10],isi[300];
 	int htg=0;
 	strcpy(nama,end);
-	while(fscanf(baca,"%s",isi)){
-		if(strstr(isi,nama)) htg++;
+	while(fscanf(baca,"%s",isi)!=EOF){
+     //  printf("%s ",isi);
+		if(strstr(isi,nama)!=NULL) htg++;
 	}
-	printf("%s %d\n",nama,htg);
-	fclose(baca);	
-}
-int main(){
-	char nama[2][10];
-	int i=0;
-	pthread_t ttt[2];
-	while(i<2) {scanf("%s",nama[i]); i++;}
- 	i=0;
-	while(i<2) {pthread_create(&(ttt[i]), NULL, &cari, (void *)nama[i]);i++;}
-	i=0;
-	while(i<2){pthread_join(ttt[i],NULL); i++;}
-
+	printf("%s  : %d\n",nama,htg);
+	fclose(baca);
 }
 
 
-
-
+int main(int argc, char *argv[]){
+//    for (int i=0;i<argc;i++) printf("%s\n", argv[i]);
+	for(int i=1;i<argc;i++) {
+        pthread_create(&(ttt[i]), NULL, &cari, (void*)argv[i]);
+    }
+	for(int i=1;i<argc;i++){
+        pthread_join(ttt[i],NULL);
+    }
+}
